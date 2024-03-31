3e2de3a6ca1ca9795d2330d25ff0da7b2073af2f
Furkan Çat <furi544@gmail.com>
2024-03-31 10:48:06 +0300

    added dropdown test logic

--- __tests__/avatar.test.jsx
+++ __tests__/avatar.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { Avatar } from "@/components/avatar";
+import Avatar from "@/components/avatar";
 
 describe("Avatar component", () => {
   beforeEach(async () => {
--- __tests__/dropdown.test.jsx
+++ __tests__/dropdown.test.jsx
@@ -1,19 +1,19 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { Dropdown } from "@/components/dropdown";
+import Dropdown from "@/components/dropdown";
 import { dropdownLabelsMockData } from "../utils/mock";
 
 describe("Dropdown component", () => {
   beforeEach(async () => {
-    const onChange = jest.fn();
-    const onClick = jest.fn();
+    const onMockChange = jest.fn();
+    const onMockClick = jest.fn();
 
     render(
       <Dropdown
         data={dropdownLabelsMockData}
-        filteredState={"React 19"}
-        onChange={onChange}
-        onClick={onClick}
+        filteredState="React 19"
+        onChange={onMockChange}
+        onClick={onMockClick}
         text="Author"
       />
     );
@@ -21,9 +21,41 @@ describe("Dropdown component", () => {
 
   describe("Test functionalities", () => {
     it("render component properly", () => {
-      const dropdown = screen.getAllByTestId("dropdown");
+      const dropdown = screen.getAllByTestId("dropdown-content");
 
       expect(dropdown).toBeDefined();
+    });
+
+    it("dropdown visibility on-click", async () => {
+      const dropdown = screen.getByTestId("dropdown-content");
+      const button = screen.getByTestId("dropdown-button");
+
+      // check is button exist & is dropdown not visible
+      expect(button).toBeDefined();
+      expect(dropdown).toHaveStyle("visibility: hidden");
+
+      // fire click event
+      fireEvent.click(button);
+      // wait till animation ends
+      await waitFor(() => expect(dropdown).toHaveStyle("visibility: visible"));
+      // check is dropdown visible
+      fireEvent.click(button);
+      // wait till animation ends
+      await waitFor(() => expect(dropdown).toHaveStyle("visibility: hidden"));
+    });
+  });
+
+  describe("Test properties", () => {
+    it("text attribute", () => {
+      const button = screen.getByTestId("dropdown-button");
+
+      expect(button).toHaveTextContent("Author");
+    });
+
+    it("filteredState attribute", () => {
+      const filteredState = screen.getAllByTestId("dropdown-filtered-state");
+
+      expect(filteredState).toBeDefined();
     });
   });
 });
