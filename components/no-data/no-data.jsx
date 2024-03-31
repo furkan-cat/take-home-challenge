cb09995a7f3eb277e0fd5bfa46d5b5a42bc0505a
Furkan Çat <furi544@gmail.com>
2024-04-01 00:12:25 +0300

    rehandle no data component, for now it indicate spinner if there is no data

--- components/no-data/no-data.jsx
+++ components/no-data/no-data.jsx
@@ -1,25 +1,14 @@
-import NextLink from "next/link";
-import { Heading, Link, Text, VStack } from "@chakra-ui/react";
-import { OctIcon } from "@/public/svg";
+import { Box, Spinner } from "@chakra-ui/react";
 
 export default function NoData() {
   return (
-    <VStack py={16} gap={4}>
-      <OctIcon w={5} h={5} fill="text.100" />
-      <Heading as="h3" size="md">
-        No results matched your search.
-      </Heading>
-      <Text color="text.200">
-        You could search{" "}
-        <Link as={NextLink} href="/" color="link.100">
-          all of GitHub
-        </Link>{" "}
-        or try an{" "}
-        <Link as={NextLink} href="/" color="link.100">
-          advanced search
-        </Link>
-        .
-      </Text>
-    </VStack>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Spinner />
+    </Box>
   );
 }
